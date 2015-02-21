1a6136d17252d4aa94f952be66d89a4b49c078da
Daniel Vartanov <dan@vartanov.net>
2015-02-21 19:07:18 +0000

    Let Rotor rotate

--- lib/enigma/rotor.rb
+++ lib/enigma/rotor.rb
@@ -1,3 +1,19 @@
 module Enigma
-  Rotor = Struct.new :encryption_map
+  class Rotor < Struct.new :encryption_map
+    def initialize
+      self.displacement = 0
+    end
+
+    def rotate
+      self.displacement = (self.displacement + 1) % 26
+    end
+
+    def position
+      ('A'.ord + displacement).chr
+    end
+
+    protected
+
+    attr_accessor :displacement
+  end
 end
---
+++ spec/enigma/rotor_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe Enigma::Rotor do
+  let(:rotor) { Rotor.new }
+
+  describe '#rotate' do
+    subject { rotor.position }
+
+    context 'initially' do
+      it { is_expected.to eq('A') }
+    end
+
+    context 'when I rotate once' do
+      before { rotor.rotate }
+
+      it { is_expected.to eq('B') }
+    end
+
+    context 'when I rotate 5 times' do
+      before { 5.times { rotor.rotate } }
+
+      it { is_expected.to eq('F') }
+    end
+
+    context 'when I rotate 25 times' do
+      before { 25.times { rotor.rotate } }
+
+      it { is_expected.to eq('Z') }
+    end
+
+    context 'when I rotate 26 times' do
+      before { 26.times { rotor.rotate } }
+
+      it { is_expected.to eq('A') }
+    end
+
+    context 'when I rotate 28 times' do
+      before { 28.times { rotor.rotate } }
+
+      it { is_expected.to eq('C') }
+    end
+  end
+end
